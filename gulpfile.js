b32d48137f2cf5f357a43aafc0b15dd575ca4994
minlare <mike@outeredgeuk.com>
2017-09-14 13:01:38 +0100

    added yarn & gulp & animate mixin

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp        = require('gulp'),
+    sass        = require('gulp-sass'),
+    sourcemaps  = require('gulp-sourcemaps'),
+    rename      = require('gulp-rename'),
+    cmq         = require('gulp-group-css-media-queries'),
+    cssmin      = require('gulp-cssmin');
+
+gulp.task('css', function() {
+    return gulp.src('sass/**/*.sass')
+        .pipe(sass({
+            errLogToConsole: true
+        }))
+        .on('error', function(error){
+            console.log(error.formatted);
+            this.emit('end');
+        })
+        .pipe(cmq())
+        .pipe(gulp.dest('css'))
+        .pipe(cssmin())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(sourcemaps.init())
+        .pipe(sourcemaps.write('maps'))
+        .pipe(gulp.dest('css'));
+});
+
+gulp.task('watch', ['css'], function(){
+    gulp.watch('sass/**/*.sass', ['css']);
+});
+
+gulp.task('default', ['css']);
